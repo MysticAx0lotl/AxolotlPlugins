e49bb84fdcf96c4f8413c7fd3ffc7290231afade
MysticAx0lotl <69944483+MysticAx0lotl@users.noreply.github.com>
2023-05-09 08:50:46 -0600

    Continue moving legacy Amogus code to LegacyAmogus
    At the request of the Vendetta team, I cannot use a cloudfare worker for this plugin. Therefore, I am rewriting the plugin to use Discord's GIF API (which I didn't even know existed before lol). While I was annoyed at first, I believe this will make for a much more functional and faster plugin, as well as make it more trustworthy as due to no longer needing an API key, I can keep 100% of the code client-side.

---
+++ plugins/legacyamogus/src/index.ts
@@ -0,0 +1,48 @@
+//--Credits--//
+//@mantikafasi for the original plugin, which can be found at https://github.com/mantikafasi/AliucordPlugins/tree/main/Amog%E2%80%8C%E2%80%8Dus
+//@BreadoWebTech for Quotes plugin, which I used as a skeleton, which can be found at https://github.com/BreadoWebTech/breadoplugs/blob/master/plugins/quotes/src/index.ts
+//Chyno Deluxe for the random GIF generator web app I used as reference material, which can be found at https://codepen.io/ChynoDeluxe/pen/WGQzWW
+//You, for downloading this plugin :D
+
+import { logger } from "@vendetta";
+
+import { registerCommand } from "@vendetta/commands";
+
+let gifCMD;
+
+
+
+
+//const quote = await getQuote();
+
+export const onLoad = () => {
+
+      gifCMD = registerCommand({
+
+          name: "amogus",
+
+          displayName: "amogus",
+
+          description: "Sends a random Among Us gif",
+
+          displayDescription: "Sends a random Among Us gif",
+
+          type: 1,
+
+          applicationId: "-1",
+
+          inputType: 1,
+
+          execute: async () => {
+            return {content: await fetch("https://amogus-plugin-worker.techguy7916261.workers.dev/").then(r=>r.text())}
+          },
+        })
+
+
+    }
+  
+
+
+export const onUnload = () => {
+  gifCMD?.();
+};
